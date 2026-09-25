55f9d2ccbe8fc73d2d376bd0d063371c5960d5dd
atplhost-cell <atplhost@gmail.com>
2026-09-25 21:46:50 +0530

    Create production admin during deployment

--- backend/build.sh
+++ backend/build.sh
@@ -3,4 +3,22 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+"
+
 python manage.py collectstatic --no-input
